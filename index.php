baabf5ea63ae1bddf6d4cf7286b7a6da2754521a
ANA BEATRIZ FERREIRA PASSOS <passos.2967@aluno.pr.senac.br>
2023-03-09 12:04:06 -0300

    tarefa

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+     $numero1 = 2;
+     $numero2 = 6;
+
+      if ($numero1 > $numero2) 
+
+      echo $numero1 . " é menor";
+
+      else 
+       echo $numero2 . " é maior";
+
+       ?>
